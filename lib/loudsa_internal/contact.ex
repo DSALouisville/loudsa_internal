de74cc407f7894ebf121bbad9a4a9135e9e18732
Chris Caragianis <chriscaragianis@gmail.com>
2017-12-05 08:55:38 -0500

    Get linting to zero, ready for branching

--- lib/loudsa_internal/contact.ex
+++ lib/loudsa_internal/contact.ex
@@ -1,8 +1,12 @@
 defmodule LoudsaInternal.Contact do
+  @moduledoc """
+  Contacts model any person whom we may wish to contact in the future. Members,
+  prospective members, media, other orgs, etc.
+  """
+
   use Ecto.Schema
   import Ecto.Changeset
   alias LoudsaInternal.Contact
-
 
   schema "contacts" do
     field :email, :string
@@ -17,6 +21,7 @@ defmodule LoudsaInternal.Contact do
     |> cast(attrs, [:name, :email])
     |> validate_required([:name, :email])
     |> validate_length(:name, min: 5)
+    # credo:disable-for-next-line Credo.Check.Readability.MaxLineLength
     |> validate_format(:email, ~r/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
   end
 end
