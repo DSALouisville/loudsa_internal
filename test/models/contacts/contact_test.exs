204ee54643187613d75d9d3f32decc46edd7eff7
Chris Caragianis <chriscaragianis@gmail.com>
2017-12-07 23:51:17 -0500

    add test for phone in contact schema

--- test/models/contacts/contact_test.exs
+++ test/models/contacts/contact_test.exs
@@ -3,9 +3,11 @@ defmodule LoudsaInternal.ContactTest do
 
   alias LoudsaInternal.Contact
 
-  @valid_attrs %{name: "Bilbo Baggins", email: "bilbo@shire.com"}
+  @valid_attrs %{name: "Bilbo Baggins", email: "bilbo@shire.com", phone: "5025551234"}
   @short_name %{name: "Bil", email: "bilbo@shire.com"}
   @bad_email %{name: "Bilbo Baggins", email: "bilboshire.com"}
+  @bad_phone %{name: "Bilbo Baggins", email: "bilboshire.com", "5551234"}
+  @fixable_phone %{name: "Bilbo Baggins", email: "bilboshire.com", " (502)  555-1234"}
 
   test "valid changeset" do
     changeset = Contact.changeset(%Contact{}, @valid_attrs)
@@ -20,5 +22,15 @@ defmodule LoudsaInternal.ContactTest do
   test "bad email" do
     changeset = Contact.changeset(%Contact{}, @bad_email)
     refute changeset.valid?
+  end
+
+  test "bad phone" do
+    changeset = Contact.changeset(%Contact{}, @bad_phone)
+    refute changeset.valid?
+  end
+
+  test "fixable phone" do
+    changeset = Contact.changeset(%Contact{}, @fixable_phone)
+    assert changeset.valid?
   end
 end
