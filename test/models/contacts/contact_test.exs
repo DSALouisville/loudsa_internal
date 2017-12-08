c98df6e4320bbcabcd06dd6a794ce8682734d1ba
Chris Caragianis <chriscaragianis@gmail.com>
2017-12-07 23:52:41 -0500

    left out a couple words

--- test/models/contacts/contact_test.exs
+++ test/models/contacts/contact_test.exs
@@ -6,8 +6,8 @@ defmodule LoudsaInternal.ContactTest do
   @valid_attrs %{name: "Bilbo Baggins", email: "bilbo@shire.com", phone: "5025551234"}
   @short_name %{name: "Bil", email: "bilbo@shire.com"}
   @bad_email %{name: "Bilbo Baggins", email: "bilboshire.com"}
-  @bad_phone %{name: "Bilbo Baggins", email: "bilboshire.com", "5551234"}
-  @fixable_phone %{name: "Bilbo Baggins", email: "bilboshire.com", " (502)  555-1234"}
+  @bad_phone %{name: "Bilbo Baggins", email: "bilboshire.com", phone: "5551234"}
+  @fixable_phone %{name: "Bilbo Baggins", email: "bilboshire.com", phone: " (502)  555-1234"}
 
   test "valid changeset" do
     changeset = Contact.changeset(%Contact{}, @valid_attrs)
